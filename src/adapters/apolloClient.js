205ff7fc6563768a00e6e6cdec03e92bc435b22a
Jordan Borja <www.jrdnbrj@gmail.com>
2021-10-27 12:36:34 -0500

    Change Production URL

--- src/adapters/apolloClient.js
+++ src/adapters/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 
 const client = new ApolloClient({
-  uri: process.env.NODE_ENV === 'production' ? 'http://localhost:8000/gql' : 'http://localhost:8000/gql',
+  uri: process.env.NODE_ENV === 'production' ? 'https://maere-server.herokuapp.com/gql' : 'http://localhost:8000/gql',
   cache: new InMemoryCache()
 })
 
