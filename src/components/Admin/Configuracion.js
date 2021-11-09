88245603efffa306d065a56b17c224741d5207b8
Jordan Borja <www.jrdnbrj@gmail.com>
2021-11-09 17:43:31 -0500

    Update texts in settings in admin

--- src/components/Admin/Configuracion.js
+++ src/components/Admin/Configuracion.js
@@ -1,19 +1,19 @@
 const Configuracion = () => {
     return <form>
-        <h1 className="display-6">Password</h1>
+        <h1 className="display-6">Contraseña</h1>
         <div className="input-group">
-            <input type="password" className="form-control" placeholder="Current Password" 
+            <input type="password" className="form-control" placeholder="Contraseña Actual" 
                 id="password-1" required />
-            <input type="password" className="form-control" placeholder="New Password" 
+            <input type="password" className="form-control" placeholder="Nueva Contraseña" 
                 id="password-2" required />
-            <input type="password" className="form-control" placeholder="New Password Again" 
+            <input type="password" className="form-control" placeholder="Nueva Contraseña Otra vez" 
                 id="password-3" required />
             {/* {passwordVisible ? 
                 <i className="bi bi-eye-slash" onClick={seePassword} /> : 
                 <i className="bi bi-eye" onClick={seePassword} />
             } */}
         </div>
-        <div className="form-text">Your password must be 8-20 characters long.</div>
+        <div className="form-text">Tu contraseña debe tener entre 8 y 20 caracteres.</div>
         {/* {changingPassword ? */}
             {/* <button type="button" className="btn btn-success mb-5" disabled>
                 <div className="spinner-border spin me-2" role="status" />
