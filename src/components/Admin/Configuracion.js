3b1f2a5bd7e541ca30b5ea96a524549918f7de5a
Jordan Borja <www.jrdnbrj@gmail.com>
2021-11-29 13:57:19 -0500

    Address Management and description on index.html

--- src/components/Admin/Configuracion.js
+++ src/components/Admin/Configuracion.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useMutation, gql } from "@apollo/client"
+import { useQuery, useMutation, gql } from "@apollo/client"
 
 
 const UPDATE_PASSWORD = gql`
@@ -10,7 +10,29 @@ const UPDATE_PASSWORD = gql`
     }
 `
 
+const UPDATE_ADDRESS = gql`
+    mutation ($address: String!) {
+        editAddress(address: $address) {
+            result
+        }
+    }
+`
+
 const Configuracion = ({ Loading }) => {
+
+    const { data } = useQuery(gql`{ getAddress }`)
+
+    const [updateAddress] = useMutation(UPDATE_ADDRESS, {
+        onCompleted: ({ editAddress: { result } }) => {
+            if (result)
+                alert('✔️ Se guardó la dirección.')
+            else alert('❌ Error al guardar la dirección. Vuelve a intentarlo.')
+        },
+        onError: ({ networkError: { result: { errors: err } } }) => {
+            console.log(err[0].message)
+            alert('❌ Error al guardar la dirección. Vuelve a intentarlo.')
+        }
+    })
 
     const [updatePassword] = useMutation(UPDATE_PASSWORD, {
         onCompleted: ({ updatePassword: { response } }) => {
@@ -33,25 +55,36 @@ const Configuracion = ({ Loading }) => {
     })
 
     const [error, setError] = useState('')
+
     const [password, setPassword] = useState('')
     const [newPassword, setNewPassword] = useState('')
     const [newPassword2, setNewPassword2] = useState('')
+
     const [passwordsValid, setPasswordsValid] = useState(true)
     const [passwordVisible, setPasswordVisible] = useState(false)
     const [changingPassword, setChangingPassword] = useState(false)
 
-    useEffect(() => {
-        if (newPassword !== newPassword2) {
-            // setError('❌ Las contraseñas no coinciden.')
-            setPasswordsValid(false)
-        }
-        else setPasswordsValid(true)
-    }, [newPassword, newPassword2])
+    const [address, setAddress] = useState('')
 
     useEffect(() => {
-        if (passwordsValid) setError('')
-        else setError('❌ Las contraseñas no coinciden.')
-    }, [passwordsValid])
+        if (data)
+            setAddress(data.getAddress)
+    }, [data])
+
+    const saveAddress = e => {
+        e.preventDefault()
+        updateAddress({ variables: { address } })
+    }
+
+    useEffect(() => {
+        if (newPassword !== newPassword2) {
+            setError('❌ Las contraseñas no coinciden.')
+            setPasswordsValid(false)
+        } else {
+            setError('')
+            setPasswordsValid(true)
+        }
+    }, [newPassword, newPassword2])
 
     const savePassword = e => {
         e.preventDefault()
@@ -80,33 +113,49 @@ const Configuracion = ({ Loading }) => {
         }
     }
 
-    return <form onSubmit={savePassword}>
-        <h1 className="display-6">Contraseña</h1>
-        <div className="input-group">
-            <input type="password" className="form-control" placeholder="Contraseña Actual" value={password} 
-                onChange={e => setPassword(e.target.value)} id="password-1" required />
-            <input type="password" className="form-control" placeholder="Nueva Contraseña" value={newPassword} 
-                onChange={e => setNewPassword(e.target.value)} id="password-2" required />
-            <input type="password" className="form-control" placeholder="Nueva Contraseña otra vez" value={newPassword2} 
-                onChange={e => setNewPassword2(e.target.value)} id="password-3" required />
-            {passwordVisible ? 
-                <i className="bi bi-eye-slash" onClick={seePassword} /> : 
-                <i className="bi bi-eye" onClick={seePassword} />
+    return <>
+        <form className="form-group" onSubmit={saveAddress}>
+            <h1 className="display-6">Dirección</h1>
+            <div className="form-floating mb-2">
+                <input className="form-control" value={address} onChange={e => setAddress(e.target.value)} />
+                <label>Dirección que aparecerá en el pie de página</label>
+            </div>
+            {changingPassword ?
+                <button type="button" className="btn btn-success mb-1" disabled>
+                    <div className="spinner-border spin me-2" role="status" />
+                    <span>Changing Password</span>
+                </button> : 
+                <button className="btn btn-sm btn-success mb-3 me-2" type="submit">Guardar</button>
             }
-        </div>
-        <div className="form-text">Tu contraseña debe tener entre 8 y 20 caracteres.</div>
-        {changingPassword ?
-            <button type="button" className="btn btn-success mb-1" disabled>
-                <div className="spinner-border spin me-2" role="status" />
-                <span>Changing Password</span>
-            </button> : 
-            <button type="submit" className="btn btn-success mb-1">
-                <span>Cambiar Contraseña</span>
-            </button>
-        }
-        {error && <div className="alert alert-danger">{error}</div>}
+        </form>
+        <form onSubmit={savePassword}>
+            <h1 className="display-6">Contraseña</h1>
+            <div className="input-group">
+                <input type="password" className="form-control" placeholder="Contraseña Actual" value={password} 
+                    onChange={e => setPassword(e.target.value)} id="password-1" required />
+                <input type="password" className="form-control" placeholder="Nueva Contraseña" value={newPassword} 
+                    onChange={e => setNewPassword(e.target.value)} id="password-2" required />
+                <input type="password" className="form-control" placeholder="Nueva Contraseña otra vez" value={newPassword2} 
+                    onChange={e => setNewPassword2(e.target.value)} id="password-3" required />
+                {passwordVisible ? 
+                    <i className="bi bi-eye-slash" onClick={seePassword} /> : 
+                    <i className="bi bi-eye" onClick={seePassword} />
+                }
+            </div>
+            <div className="form-text">Tu contraseña debe tener entre 8 y 20 caracteres.</div>
+            {changingPassword ?
+                <button type="button" className="btn btn-sm btn-success mb-1" disabled>
+                    <div className="spinner-border spin me-2" role="status" />
+                    <span>Changing Password</span>
+                </button> : 
+                <button type="submit" className="btn btn-sm btn-success mb-1">
+                    <span>Cambiar Contraseña</span>
+                </button>
+            }
+            {error && <div className="alert alert-danger">{error}</div>}
 
-    </form>
+        </form>
+    </>
 }
 
 export default Configuracion
